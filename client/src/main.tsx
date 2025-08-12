cdecde22689a6156e42399be950b6b825f0412c2
khanh dinh <45358938-khanhdd@users.noreply.replit.com>
2025-08-12 13:07:08 +0000

    Restored to '2045642b0a67df010e929682ef97e0648ebab1cf'
    Replit-Restored-To: 2045642b0a67df010e929682ef97e0648ebab1cf

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,5 @@
-
-import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const rootElement = document.getElementById("root");
-if (!rootElement) {
-  throw new Error("Root element not found");
-}
-
-const root = createRoot(rootElement);
-root.render(<App />);
+createRoot(document.getElementById("root")!).render(<App />);
